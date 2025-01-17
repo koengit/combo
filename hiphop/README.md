7167afd2507d0dadb300e16de323c62bffe7f5fb
Manuel Serrano <Manuel.Serrano@inria.fr>
2025-01-17 19:26:40 +0100

    HipHop p15 example

---
+++ hiphop/README.md
@@ -0,0 +1,21 @@
+This directory contains HipHop examples of programs and generated circuits.
+To execute these programs, one needs to install nodejs and npm and and
+graphviz to draw PDF circuits.
+
+In order to run the tests contained in this directory:
+
+```
+npm install
+```
+
+Then, to test the net list generation:
+
+```
+npm test-json
+```
+
+To test the PDF generation:
+
+```
+npm  test-pdf
+```
---
+++ hiphop/p15/p15.hh.mjs
@@ -0,0 +1,40 @@
+// P15.hh.mjs
+// Constructive Semantics of Pure Esterel, page 74
+import * as hh from "@hop/hiphop";
+
+hiphop module P15() {
+   in I, J;
+   out O1, O2, O3;
+
+   fork {
+      if (I.now) {
+	 emit O1(1);
+      }
+   } par {
+      if (J.now) {
+	 if (O1.now) {
+	    emit O2(2);
+	 } else {
+	    emit O3(3);
+	 }
+      }
+   }
+}
+
+export const mach = new hh.ReactiveMachine(P15, { sweep: true, dumpNets: true, verbose: 1 });
+mach.outbuf = "";
+
+mach.addEventListener("O3", v => mach.outbuf += "O3=" + v.nowval + "\n");
+mach.addEventListener("O2", v => mach.outbuf += "O2=" + v.nowval + "\n");
+mach.addEventListener("O1", v => mach.outbuf += "O1=" + v.nowval + "\n");
+
+function react(sigs) {
+   const s = mach.save();
+   mach.react(sigs);
+   mach.restore(s);
+}
+
+react({I: 1});
+react({I: 1, J: 1});
+react({J: 1});
+react();
